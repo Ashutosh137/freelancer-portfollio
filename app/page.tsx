8ca7833cb8edc76da7f9e393528243d53ad7b71a
ashutosh <mr.luckysharma7@gmail.com>
2024-07-11 11:47:43 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,83 @@
-import { ImagesSlider } from "@/components/image-slider";
+"use client"
+import { ImageSlider } from "@/components/image-slider";
 import image1 from '@/public/1.jpg'
-import image2 from '@/public/poster 2024_1.jpg'
+// import image2 from '@/public/poster 2024_1.jpg'
 import image3 from '@/public/3.jpg'
 import image4 from '@/public/4.jpg'
 import image5 from '@/public/5.jpg'
 import image6 from '@/public/6.jpg'
+import { motion } from "framer-motion";
+import { SparklesCore } from "@/components/sprkals";
+import Navbar from "@/components/navbar";
+import Marquee from "@/components/marcos";
+import { LampDemo } from "@/components/lamp";
 export default function Home() {
   return (
-    <div className="text-">
-hjsbkjskjbs
-hjsbkjskjbs
-hjsbkjskjbs
-    <ImagesSlider images={[image1,image2,image3,image4,image5,image6]}>
-      <>hello</>
-    </ImagesSlider>
+    <div className=" ">
+      <Navbar />
+      <SparklesCore background={"re"} maxSize={2} className="w-full fixed inset-0  bg-opacity-40 -z-10" />
+      <ImageSlider className="h-[90vh]  w-full" images={[image1, image3, image4, image5, image6]}>
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: -80,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 0.6,
+          }}
+          className="z-50 w-full absolute inset-0 flex flex-col justify-center items-center"
+        >
+          <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+            The hero section slideshow <br /> nobody asked for
+          </motion.p>
+          <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+            <span>Join now →</span>
+            <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+          </button>
+        </motion.div>
+      </ImageSlider>
+
+
+      <Marquee pauseOnHover repeat={5}>
+
+        <article className="mx-10 max-w-md border rounded-xl p-5 py-3">
+          <div className="flex  items-center mb-4">
+            <img className="w-10 h-10 me-4 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+            <div className="font-medium dark:text-white">
+              <p>Jese Leos <time className="block text-sm text-gray-500 dark:text-gray-400">Joined on August 2014</time></p>
+            </div>
+          </div>
+          <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg className="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          </div>
+          <aside>
+            <p className="mt-2 text-xs text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut minus nam, harum architecto officiis voluptatum dolorem ex recusandae fuga delectus?</p>
+
+          </aside>
+        </article>
+      </Marquee>
+
+
+      <LampDemo />
+
     </div>
   );
 }
--- components/image-slider.tsx
+++ components/image-slider.tsx
@@ -1,10 +1,21 @@
+// components/ImageSlider.tsx
 "use client";
 import { cn } from "@/utiles/cn";
 import { motion, AnimatePresence } from "framer-motion";
 import Image, { StaticImageData } from "next/image";
 import React, { useState, useEffect } from "react";
 
-export const ImagesSlider = ({
+interface ImageSliderProps {
+  images: StaticImageData[];
+  children?: React.ReactNode;
+  overlay?: React.ReactNode;
+  overlayClassName?: string;
+  className?: string;
+  autoplay?: boolean;
+  direction?: "up" | "down";
+}
+
+export const ImageSlider = ({
   images,
   children,
   overlay = true,
@@ -12,16 +23,9 @@ export const ImagesSlider = ({
   className,
   autoplay = true,
   direction = "down",
-}: {
-  images: StaticImageData[];
-  children: React.ReactNode;
-  overlay?: React.ReactNode;
-  overlayClassName?: string;
-  className?: string;
-  autoplay?: boolean;
-  direction?: "up" | "down";
-}) => {
+}: ImageSliderProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) =>
@@ -37,6 +41,8 @@ export const ImagesSlider = ({
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
+      intervalId && clearInterval(intervalId)
+
       if (event.key === "ArrowRight") {
         handleNext();
       } else if (event.key === "ArrowLeft") {
@@ -47,81 +53,57 @@ export const ImagesSlider = ({
     window.addEventListener("keydown", handleKeyDown);
 
     // autoplay
-    let interval: any;
     if (autoplay) {
-      interval = setInterval(() => {
-        handleNext();
-      }, 5000);
+      const id = setInterval(handleNext, 5000);
+      setIntervalId(id);
     }
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
-      clearInterval(interval);
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
     };
-  }, [autoplay]);
-
-  const slideVariants = {
-    initial: {
-      scale: 0,
-      opacity: 0,
-      rotateX: 45,
-    },
-    visible: {
-      scale: 1,
-      rotateX: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        ease: [0.645, 0.045, 0.355, 1.0],
-      },
-    },
-    upExit: {
-      opacity: 1,
-      y: "-150%",
-      transition: {
-        duration: 1,
-      },
-    },
-    downExit: {
-      opacity: 1,
-      y: "150%",
-      transition: {
-        duration: 1,
-      },
-    },
-  };
+  }, [autoplay, intervalId]);
 
   return (
-    <div
-      className={cn(
-        "overflow-hidden h-full w-full relative flex items-center justify-center",
-        className
-      )}
-      style={{
-        perspective: "1000px",
-      }}
-    >
-      {children}
-      {overlay && (
-        <div
-          className={cn("absolute inset-0 bg-black/60 z-40", overlayClassName)}
-        />
-      )}
+    <div>
 
-      <AnimatePresence>
-        <motion.div
-          key={currentIndex}
-          initial="initial"
-          animate="visible"
-          exit={direction === "up" ? "upExit" : "downExit"}
-          variants={slideVariants}
-          className="w-full h-screen"
-        >
-          <Image 
-          className="image h-full w-full  absolute inset-0 object-cover object-center"
-          src={images[currentIndex]} alt="aknd"/>
-        </motion.div>
-      </AnimatePresence>
+    <div className={cn(className, "relative w-full ")}>
+     
+      <div className="relative overflow-hidden w-full h-full">
+        <AnimatePresence >
+          <motion.div
+            key={currentIndex}
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
+            className="absolute w-full inset-0 h-full rounded"
+          >
+            <Image
+              className="w-full h-full object- object-center"
+              src={images[currentIndex]}
+              alt={`Slide ${currentIndex}`}
+            // layout=""
+            />
+          </motion.div>
+        </AnimatePresence>
+      </div>
+      
+      {overlay && (
+        <div className={cn("absolute bg-black/55 inset-0 z-10", overlayClassName)}>{overlay}</div>
+      )}
+      {children}
+
+    </div>
+
+    <div className="w-full flex pt-10">
+      <div className="flex justify-between w-80 ml-auto mr-10">
+        <button className="border p-2 border-red-300">left</button>
+        <button>right</button>
+      </div>
+    </div>
     </div>
   );
 };
